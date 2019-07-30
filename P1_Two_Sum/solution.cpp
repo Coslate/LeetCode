9d504617f9cb71f987ff3e96843492f3cabdc1ce
Coslate <coslate@media.ee.ntu.edu.tw>
2019-07-31 00:07:15 +0800

    Update another method

--- P1_Two_Sum/solution.cpp
+++ P1_Two_Sum/solution.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <solution.h>
 
+/*
 std::vector<int> Solution::twoSum(std::vector<int>& nums, int target) {
     std::map<int, int> hash_map;
     std::vector<int>::iterator vec_begin = nums.begin();
@@ -18,6 +19,26 @@ std::vector<int> Solution::twoSum(std::vector<int>& nums, int target) {
             return answer_vec;
         }else{
             hash_map[*it] = it - vec_begin;
+        }
+    }
+
+    printf("Error: No two sum solution.\n");
+    exit (EXIT_FAILURE);
+}
+*/
+std::vector<int> Solution::twoSum(std::vector<int>& nums, int target) {
+    std::map<int, int> hash_map;
+
+    for (size_t i=0, vec_size=nums.size(); i!=vec_size; ++i){
+        int complement = target - nums[i];
+
+        if (hash_map.find(complement)!=hash_map.end()) {
+            std::vector<int> answer_vec (2, 0);
+            answer_vec[0] = hash_map.find(complement)->second;
+            answer_vec[1] = i;
+            return answer_vec;
+        }else{
+            hash_map[nums[i]] = i;
         }
     }
 
