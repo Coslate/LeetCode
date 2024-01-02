e4c7990644e07c2e879ac71ebdd022a7144ced6c
Coslate <coslate@media.ee.ntu.edu.tw>
2024-01-02 23:39:53 +0800

    Initialize P73.

---
+++ P73_Set_Matrix_Zeroes/Python/my_imp/solution.py
@@ -0,0 +1,19 @@
+from typing import Optional, List
+import collections
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        # Matrix | Time: O(n^2) | Space: O(n)
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        pass
+
+
+class OptSolution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        # Matrix | Time: O(n^2) | Space: O(n)
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        pass
