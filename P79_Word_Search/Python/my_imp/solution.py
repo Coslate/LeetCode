cf1a1199b606813204f9d319ae20dc73e9ce2b50
Coslate <coslate@media.ee.ntu.edu.tw>
2024-02-04 20:04:27 +0800

    Minor fix of P79/

--- P79_Word_Search/Python/my_imp/solution.py
+++ P79_Word_Search/Python/my_imp/solution.py
@@ -4,6 +4,13 @@ import math
 
 class Solution:
     def DFSCall(self, board: List[List[str]], word: str, i: int, j:int, word_idx: int, got_mask: List[List[bool]], visit: List[List[bool]]) -> (bool, int):
+        visit[i][j] = True
+        ans = []
+
+        while True:
+            pass
+
+        return (ans, word_idx)
     def exist(self, board: List[List[str]], word: str) -> bool:
         # Matrix | Time: O(n^2) | Space: O(1)
         row_num   = len(board)
@@ -15,17 +22,16 @@ class Solution:
         for i in range(row_num):
             for j in range(col_num):
                 if visit[i][j] == False:
-                    (ans, word_idx) = DFSCall(board, word, i, j, word_idx, got_mask, visit)
+                    (ans, word_idx) = self.DFSCall(board, word, i, j, word_idx, got_mask, visit)
                 elif visit[i][j] == True and got_mask[i][j] == True:
                     if i+1 < row_num and visit[i+1][j] == False:
-                        (ans, word_idx) = DFSCall(board, word, i+1, j, word_idx, got_mask, visit)
+                        (ans, word_idx) = self.DFSCall(board, word, i+1, j, word_idx, got_mask, visit)
                     elif j+1 < col_num and visit[i][j+1] == False:
-                        (ans, word_idx) = DFSCall(board, word, i, j+1, word_idx, got_mask, visit)
+                        (ans, word_idx) = self.DFSCall(board, word, i, j+1, word_idx, got_mask, visit)
                     elif i-1 >= 0 and visit[i-1][j] == False:
-                        ans = DFSCall(board, word, i-1, j)
-                        (ans, word_idx) = DFSCall(board, word, i-1, j, word_idx, got_mask, visit)
+                        (ans, word_idx) = self.DFSCall(board, word, i-1, j, word_idx, got_mask, visit)
                     elif j-1 >= 0 and visit[i][j-1] == False:
-                        (ans, word_idx) = DFSCall(board, word, i, j-1, word_idx, got_mask, visit)
+                        (ans, word_idx) = self.DFSCall(board, word, i, j-1, word_idx, got_mask, visit)
 
 class OptSolution:
     def exist(self, board: List[List[str]], word: str) -> bool:
