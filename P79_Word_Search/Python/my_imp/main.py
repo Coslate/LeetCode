77fdd22b9981ba89a75750d4baada2ed548cee29
Coslate <coslate@media.ee.ntu.edu.tw>
2024-02-14 21:54:27 +0800

    Add more testcase.

--- P79_Word_Search/Python/my_imp/main.py
+++ P79_Word_Search/Python/my_imp/main.py
@@ -73,6 +73,57 @@ def main():
     print(f"")
     print(f"")
 
+    board =  [
+                ["a","b"],
+                ["c","d"]
+            ]
+    word = "acdb"
+
+    print(f"//Case5:")
+    print(f"//------Original-------//")
+    print(f"board = {board}")
+    print(f"word  = {word}")
+    print(f"//------Checked-------//")
+    ans = sol.exist(board, word)
+    print(f"ans = {ans}")
+    print(f"")
+    print(f"")
+
+    board = [
+                ["C","A","A"],
+                ["A","A","A"],
+                ["B","C","D"]
+            ]
+
+    word = "AAB"
+
+    print(f"//Case6:")
+    print(f"//------Original-------//")
+    print(f"board = {board}")
+    print(f"word  = {word}")
+    print(f"//------Checked-------//")
+    ans = sol.exist(board, word)
+    print(f"ans = {ans}")
+    print(f"")
+    print(f"")
+
+    board = [
+                ["A","B","C","E"],
+                ["S","F","E","S"],
+                ["A","D","E","E"]
+            ]    
+    word = "ABCESEEEFS"
+
+    print(f"//Case7:")
+    print(f"//------Original-------//")
+    print(f"board = {board}")
+    print(f"word  = {word}")
+    print(f"//------Checked-------//")
+    ans = sol.exist(board, word)
+    print(f"ans = {ans}")
+    print(f"")
+    print(f"")
+
 
 #---------------Execution---------------#
 if __name__ == '__main__':
--- P79_Word_Search/Python/my_imp/solution.py
+++ P79_Word_Search/Python/my_imp/solution.py
@@ -10,25 +10,51 @@ class Solution:
         word_idx  = 0
         word_len  = len(word)
         ans       = False
+        ans_list  = []
 
         while len(stack_dfs) > 0:
             (idx_i, idx_j) = stack_dfs.pop()
             visit[idx_i][idx_j] = True
+            #print(f"pop({idx_i}, {idx_j})")
 
             if board[idx_i][idx_j] == word[word_idx]:
+                ans_list.append((idx_i, idx_j))
                 word_idx += 1
                 if word_idx == word_len:
                     ans = True
                     break
 
-                if idx_i+1 < row_num and visit[idx_i+1][idx_j] == False:
-                    stack_dfs.append((idx_i+1, idx_j))
-                if idx_j+1 < col_num and visit[idx_i][idx_j+1] == False:
-                    stack_dfs.append((idx_i, idx_j+1))
-                if idx_i-1 > -1 and visit[idx_i-1][idx_j] == False:
-                    stack_dfs.append((idx_i-1, idx_j))
-                if idx_j-1 > -1 and visit[idx_i][idx_j-1] == False:
-                    stack_dfs.append((idx_i, idx_j-1))
+                check_idx_i = idx_i+1
+                check_idx_j = idx_j
+                if check_idx_i < row_num and visit[check_idx_i][check_idx_j] == False and board[check_idx_i][check_idx_j]==word[word_idx]:
+                    stack_dfs.append((check_idx_i, check_idx_j))
+                    down_got = True
+                    #print(f"append: ({check_idx_i}, {check_idx_j})")
+
+                check_idx_i = idx_i
+                check_idx_j = idx_j+1
+                if check_idx_j < col_num and visit[check_idx_i][check_idx_j] == False and board[check_idx_i][check_idx_j]==word[word_idx]:
+                    stack_dfs.append((check_idx_i, check_idx_j))
+                    right_got = True
+                    #print(f"append: ({check_idx_i}, {check_idx_j})")
+
+                check_idx_i = idx_i-1
+                check_idx_j = idx_j
+                if check_idx_i > -1 and visit[check_idx_i][check_idx_j] == False and board[check_idx_i][check_idx_j]==word[word_idx]:
+                    stack_dfs.append((check_idx_i, check_idx_j))
+                    up_got = True
+                    #print(f"append: ({check_idx_i}, {check_idx_j})")
+
+                check_idx_i = idx_i
+                check_idx_j = idx_j-1
+                if check_idx_j > -1 and visit[check_idx_i][check_idx_j] == False and board[check_idx_i][check_idx_j]==word[word_idx]:
+                    stack_dfs.append((check_idx_i, check_idx_j))
+                    left_got = True
+                    #print(f"append: ({check_idx_i}, {check_idx_j})")
+
+                if (not up_got) and (not down_got) and (not left_got) and (not right_got):
+                    (_i, _j) = stack_dfs.pop()
+                    (_k, _m) = ans_list.pop()
 
         return ans
 
@@ -36,11 +62,11 @@ class Solution:
         # Matrix | Time: O(n^2) | Space: O(1)
         row_num   = len(board)
         col_num   = len(board[0])
-        visit     = [[False for j in range(col_num)] for i in range(row_num)]
 
         for i in range(row_num):
             for j in range(col_num):
-                if visit[i][j] == False:
+                if board[i][j] == word[0]:
+                    visit = [[False for j in range(col_num)] for i in range(row_num)]
                     ans = self.DFSCall(board, word, i, j, visit, row_num, col_num)
                     if ans:
                         return True
