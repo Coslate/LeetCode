c3f69485da13e363ba5efd67df2713c818651bd8
Coslate <coslate@media.ee.ntu.edu.tw>
2024-09-04 05:50:16 +0000

    Initialize P572.

---
+++ P572_Subtree_of_Another_Tree/Python/my_imp/main.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+from solution import *
+
+def main():
+    sol = Solution()
+    sol_opt = OptSolution()
+
+    print(f"//Case1:")
+    root = [1,2,3,'null','null',4,5]
+    print(f"//------Original-------//")
+    print(f"root = {root}")
+    print(f"//------Checked-------//")
+    print(f"")
+    print(f"")
+
+
+#---------------Execution---------------#
+if __name__ == '__main__':
+    main()
