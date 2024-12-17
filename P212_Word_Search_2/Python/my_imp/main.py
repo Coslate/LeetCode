aceeefd875ff2450fd9b2d3f8b33d113ec3a00c5
Coslate <coslate@media.ee.ntu.edu.tw>
2024-12-17 05:51:24 +0000

    Init.

---
+++ P212_Word_Search_2/Python/my_imp/main.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+from solution import *
+
+def main():
+    print(f"//Case1:")
+    board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
+    words = ["oath","pea","eat","rain"]
+    ans = Solution().findWords(board=board, words=words)
+    print(f"ans = {ans}")
+
+#---------------Execution---------------#
+if __name__ == '__main__':
+    main()
