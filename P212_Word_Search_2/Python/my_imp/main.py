a94e6dcda0463b5009999382ab5218da1f16314c
Coslate <coslate@media.ee.ntu.edu.tw>
2025-01-08 17:33:40 +0000

    Complete P121 Python.

--- P212_Word_Search_2/Python/my_imp/main.py
+++ P212_Word_Search_2/Python/my_imp/main.py
@@ -8,6 +8,11 @@ def main():
     words = ["oath","pea","eat","rain"]
     ans = Solution().findWords(board=board, words=words)
     print(f"ans = {ans}")
+    print(f"//Case2:")
+    board = [["a","b"],["c","d"]]
+    words = ["abcb"]
+    ans = Solution().findWords(board=board, words=words)
+    print(f"ans = {ans}")
 
 #---------------Execution---------------#
 if __name__ == '__main__':
